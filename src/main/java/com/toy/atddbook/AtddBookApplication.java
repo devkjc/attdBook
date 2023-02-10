fd33ee2ab301189c9c6bf3743d503d0b312ca291
JongChan <kjc3590@gmail.com>
2023-02-10 16:15:58 +0900

    init: ATDD

---
+++ src/main/java/com/toy/atddbook/AtddBookApplication.java
@@ -0,0 +1,13 @@
+package com.toy.atddbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/atddbook/AtddBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.atddbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
